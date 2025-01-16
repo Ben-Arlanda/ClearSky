9793c8986a59b700f90fe5e7e2921062f95a18b7
Ben-Arlanda <ben.arlanda@protonmail.com>
2025-01-16 18:52:54 +1100

    added button to dashbaord and styling, added new api key and added .env to gitignore

--- src/pages/WeatherDashboard.tsx
+++ src/pages/WeatherDashboard.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { Button } from "@/components/ui/button";
+import { RefreshCw } from "lucide-react";
+import { useGeolocation } from "@/hooks/useGeolocation";
 
 const WeatherDashboard = () => {
-  return <div></div>;
+	const {
+		coordinates,
+		error: locationError,
+		isLoading: locationLoading,
+		getLocation,
+	} = useGeolocation();
+
+	console.log(coordinates);
+
+	return (
+		<div className="space-y-4">
+			<div className="flex items-center justify-between">
+				<h1 className="text-xl font-bold tracking-tight">My Location</h1>
+				<Button variant={"outline"} size={"icon"}>
+					<RefreshCw className="h-4 w-4" />
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default WeatherDashboard;
